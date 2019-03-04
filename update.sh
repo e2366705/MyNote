666e162d4e524f2b2d90494b46bc6b2103038625
e2366705 <2366705745@qq.com>
2019-03-04 20:38:04 +0800

    Updata...

---
+++ update.sh
@@ -0,0 +1,8 @@
+echo '------- step 1 ---------'
+git rm -f --cached .
+echo '------- step 2 ---------'
+git add -f *
+echo '------- step 3 ---------'
+git commit -m 'Updata...'
+echo '------- step 4 ---------'
+git push -u origin master
